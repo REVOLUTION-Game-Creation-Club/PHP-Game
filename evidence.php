c13720d859c0a867e4f44e694b3ac29519810b91
jillsadowski <jillsadowski@gmail.com>
2016-07-18 12:43:00 -0400

    First renderings of using php in evidence page. edited css

--- evidence.php
+++ evidence.php
@@ -1,5 +1,23 @@
 
-<?php include("header.php"); ?>
+<?php include("header.php"); 
+include("connection.php");
+
+?>
+
+<ul>
+<?php foreach ($suspects as $item) {
+    echo "<li><img class='image' src='img/"
+        .$item["image"]
+        ."' alt='" 
+        . $item["name"] 
+        . "' /> <br> <div class='name'>" 
+        . $item["name"]
+        . " </div><br><div class='content'>" 
+        . $item["background"]
+        . "</div> </li>";
+} ?>
+
+</ul>
 
 
 
